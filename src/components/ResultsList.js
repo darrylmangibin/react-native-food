1bcba66a6ad71c9d92d432fbd0d06552bf2efcac
darryl <darrylmangibin@gmail.com>
2020-02-27 15:09:17 +0800

    finalized

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React from "react";
+import {
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity
+} from "react-native";
+import { withNavigation } from "react-navigation";
 
-import ResultsDetail from '../components/ResultsDetail';
+import ResultsDetail from "../components/ResultsDetail";
 
-const ResultsList = ({ title, results }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <FlatList 
-        showsHorizontalScrollIndicator={false}
-        data={results}
-        horizontal
-        keyExtractor={result => result.id}
-        renderItem={({ item }) => {
-          return (
-            <ResultsDetail result={item} />
-          )
-        }}
-      />
-    </View>
-  )
-}
+const ResultsList = ({ title, results, navigation }) => {
+	if(!results.length) {
+		return null
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>{title}</Text>
+			<FlatList
+				showsHorizontalScrollIndicator={false}
+				data={results}
+				horizontal
+				keyExtractor={result => result.id}
+				renderItem={({ item }) => {
+					return (
+						<TouchableOpacity
+							onPress={() =>
+								navigation.navigate("ResultsShow", { id: item.id })
+							}
+						>
+							<ResultsDetail result={item} />
+						</TouchableOpacity>
+					);
+				}}
+			/>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    marginBottom: 10
-  },  
+	container: {
+		marginBottom: 10
+	},
 	title: {
 		fontSize: 18,
 		fontWeight: "bold",
-    marginLeft: 15,
-    marginBottom: 5
+		marginLeft: 15,
+		marginBottom: 5
 	}
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -27,14 +27,17 @@ const SearchScreen = () => {
 					results={filterResultsByPrice("₱")}
 					title="Cost Effective"
 				/>
-				<ResultsList results={filterResultsByPrice("₱₱")} title="Bit Pricier" />
+				<ResultsList
+					results={filterResultsByPrice("₱₱")}
+					title="Bit Pricier"
+				/>
 				<ResultsList
 					results={filterResultsByPrice("₱₱₱")}
 					title="Big Spender"
 				/>
 				<ResultsList
 					results={filterResultsByPrice("₱₱₱₱")}
-					title="Big Spender"
+					title="Pricy"
 				/>
 			</ScrollView>
 		</>
